1371304416de5bfdecf035dafd33c665a97c886a
Meet Vekariya <mvekariya398@gmail.com>
2026-09-19 16:35:59 +0530

    feat: add WebSocket support with connection management, message editor, and logs viewer

--- app/(workspace)/layout.tsx
+++ app/(workspace)/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "@/modules/layout/components/header";
 import { currentUser } from "@/modules/authentication/actions";
 import { initializeWorkspace } from "@/modules/workspace/actions";
+import TabbedLeftPanel from "@/modules/workspace/components/tabbed-left-panel";
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
     const workspace = await initializeWorkspace()
@@ -12,9 +13,9 @@ const RootLayout = async ({ children }: { children: React.ReactNode }) => {
             <Header user={user} />
             <main className="max-h-[calc(100vh-4rem)] h-[calc(100vh-4rem)] flex flex-1 overflow-hidden">
                 <div className="flex h-full w-full">
-                    {/* <div className="w-12 border-zinc-800 bg-zinc-900">
-                        tabbadleft panel
-                    </div> */}
+                    <div className="w-12 border-zinc-800 bg-zinc-900">
+                        <TabbedLeftPanel />
+                    </div>
                     <div className="flex-1 bg-zinc-900">
                         {children}
                     </div>
